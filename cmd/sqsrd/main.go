7eb9c3decd7b52d79bf43e0d69c0fc3a26af5e82
Jason Dsouza <jasonrdsouza@gmail.com>
2020-03-29 14:06:37 -0400

    Remove unnecessary context

--- cmd/sqsrd/main.go
+++ cmd/sqsrd/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"strconv"
-	"time"
 
 	"dsouza.io/sqsrd"
 	"github.com/aws/aws-sdk-go/aws"
@@ -29,12 +27,10 @@ func injectFakeMessages(numFakeMessages int, channel chan<- *sqs.Message) {
 func main() {
 	var redriver sqsrd.SqsRedriver
 	var region, profile string
-	var timeout time.Duration
 	var parallelism, bufferSize int
 
 	flag.StringVar(&redriver.SourceQueue, "source", "", "Source Queue URL.")
 	flag.StringVar(&redriver.DestQueue, "dest", "", "Destination Queue URL.")
-	flag.DurationVar(&timeout, "timeout", time.Minute*5, "Upload timeout.")
 	flag.IntVar(&redriver.MaxEmptyReceives, "emptyReceives", 3, "Maximum empty message receives.")
 	flag.IntVar(&parallelism, "parallelism", 1, "Parallelism to run with.")
 	flag.IntVar(&bufferSize, "buffer", 100, "Size of message buffer to keep in memory.")
@@ -48,19 +44,6 @@ func main() {
 	}))
 
 	redriver.Svc = sqs.New(sess)
-
-	// Create a context with a timeout that will abort the upload if it takes
-	// more than the passed in timeout.
-	ctx := context.Background()
-	var cancelFn func()
-	if timeout > 0 {
-		ctx, cancelFn = context.WithTimeout(ctx, timeout)
-	}
-	// Ensure the context is canceled to prevent leaking.
-	// See context package for more information, https://golang.org/pkg/context/
-	if cancelFn != nil {
-		defer cancelFn()
-	}
 
 	messagesToRedrive := make(chan *sqs.Message, bufferSize)
 	go redriver.ReceiveMessages(messagesToRedrive, parallelism)
